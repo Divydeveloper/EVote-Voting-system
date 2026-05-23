73e99886ee834e1bf86a0b663d1ea9712b55767c
Divyansh Gaur <gaurdivyansh2005@gmail.com>
2026-05-23 23:59:51 +0530

    Connected render with vercel

--- evote/frontend/src/utils/api.js
+++ evote/frontend/src/utils/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://evote-backend-83mn.onrender.com',
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
 });
