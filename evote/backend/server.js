c4f2e5cfef64e5c715e1bbb7effa979a4744d43b
Divyansh Gaur <gaurdivyansh2005@gmail.com>
2026-05-24 01:18:01 +0530

    Update server.js

--- evote/backend/server.js
+++ evote/backend/server.js
@@ -29,6 +29,7 @@ app.use(cors({
     "https://e-vote-voting-system.vercel.app",
     "https://e-vote-voting-system-git-main-divyanshs-projects-b81ac839.vercel.app"
   ],
+  credentials:true;
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
